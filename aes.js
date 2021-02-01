4c47862abd043b4c8478f66194ff5347a44a18a3
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-01 19:59:41 +0530

    init commit

---
+++ aes.js
@@ -0,0 +1,15 @@
+const encrypt = require('./lib/encrypt');
+const decrypt = require('./lib/decrypt');
+
+const [mode, file, password] = process.argv.slice(2);
+const shouldEncrypt = mode === 'encrypt';
+const shouldDecrypt = mode === 'decrypt';
+
+if (shouldEncrypt) {
+    encrypt({ file, password });
+}
+
+if (shouldDecrypt) {
+    decrypt({ file, password });
+    console.log({ file });
+}
